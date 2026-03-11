d636b8b9deadbb30d2cf0ae40da3b08d5f27cfc5
Rakkuny <estefaniamedellinperez.unam@gmail.com>
2026-03-11 12:51:47 -0600

    Nueva Scena y Fin corrutina

--- Assets/Script/Concurrencia.cs
+++ Assets/Script/Concurrencia.cs
@@ -16,7 +16,7 @@ public class Concurrencia : MonoBehaviour {
 
     [Header("Esfera a mover")]
     public Transform sincronoSphere;
-    public Transform ThreadSphere;
+    public Transform threadSphere;
     public Transform taskSphere;
     public Transform corutineSphere;
     public Transform mainCube;
@@ -24,8 +24,13 @@ public class Concurrencia : MonoBehaviour {
     //Acciones a ejecutar en el hilo secundario, en este caso se uso una cola
     private Queue<Action> mainThreadActions = new Queue<Action>();
 
-    void Start() {
-
+    void Start() 
+    {
+        //ejecutar la bandera actuvas, solo las que esten en true
+        if(useSincrono) MoveSincrono();
+        if(useThread) MoveWithThread();
+        if(useTask) MoveWithTask();
+        if(useCorutine) StartCoroutine(MoveWithCorutine());
     }
 
     void Update() {
@@ -33,9 +38,11 @@ public class Concurrencia : MonoBehaviour {
         mainCube.Rotate(Vector3.up, 50 * Time.deltaTime);
 
         //ejecuta las acciones en el hilo principal
-        lock (mainThreadActions) {
+        lock (mainThreadActions) 
+        {
             //bloquear, si hay acciones que realizar, la saca de la cola y ejecuta la accion.
-            while (mainThreadActions.Count > 0) {
+            while (mainThreadActions.Count > 0) 
+            {
                 mainThreadActions.Dequeue().Invoke();
             }
         }
@@ -51,8 +58,54 @@ public class Concurrencia : MonoBehaviour {
         Thread.Sleep(50);
     }
 
+    //movimiento con hilo secundario 
+    void MoveWithThread() 
+    {
+        //lamnda => significa "tal que", todo dentro de este hilo se ejecuta en un proceso secundario.
+        new Thread(() => 
+        {
+            //que el hilo mueva la esfera
+            for (int i = 0; i <= 100; i++) 
+            {
+                Thread.Sleep(50);
+                lock (mainThreadActions) //hay que bloquear el hilo, o consumira muchos recursos sin saber si se cerro el hilo o no
+                {
+                    mainThreadActions.Enqueue(() => 
+                    {
+                        //movimiento que corresponde a desplazar a la esferea con el vector
+                        threadSphere.position += Vector3.right * 0.05f;
+                    });
+                }
+            }
+        }).Start(); //inicialo
+    }
+    //si no se bloquea el hilo es como un virus
+
+
+    //metodo con task asincrono
+    async void MoveWithTask()
+    {
+        await Task.Run(() => 
+        {
+            Thread.Sleep(50);
+            for (int i = 0; i <= 100; i++) 
+            {
+                //mov de seuridad
+                lock (mainThreadActions) 
+                {
+                    mainThreadActions.Enqueue(() => 
+                    {
+                        taskSphere.position += Vector3.right * 0.05f;
+                    });
+                }
+            }
+        });
+    }
+
+ 
+
     //corrutina
-    IEnumerator MoveWhitCorutine()
+    IEnumerator MoveWithCorutine()
     {
         corutineSphere.position += Vector3.right * 0.05f;
         yield return new WaitForSeconds(0.05f);
